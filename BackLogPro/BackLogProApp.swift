25bb1a46d6813148b6762a4d1394009d897e3e07
Joshua Basche <pompousair@Joshuas-MacBook-Air.local>
2022-01-26 18:27:59 -0800

    initial commit

---
+++ BackLogPro/BackLogProApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackLogProApp.swift
+//  BackLogPro
+//
+//  Created by Joshua Basche on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BackLogProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
